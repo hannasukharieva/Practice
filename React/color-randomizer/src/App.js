8f7b777f3647685b96a2fbc625665b1588187e51
hannasukharieva <annsukhareva33@gmail.com>
2022-03-20 21:49:32 +0000

    Initial commit

---
+++ React/color-randomizer/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Random from "./components/Random";
+
+function App() {
+  return (
+    <div className="App">
+      <Random />
+    </div>
+  );
+}
+
+export default App;
