554b3191569550be8d1e00364bedecce990d0b49
hannasukharieva <hsukharieva@gmail.com>
2022-04-03 10:16:02 +0100

    Add build script

--- Node/server/index.js
+++ Node/server/index.js
@@ -1,11 +1,15 @@
 // server/index.js
 
 const express = require("express");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 //Создает переменную и помещает в нее express сервер
 const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -14,3 +18,9 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
